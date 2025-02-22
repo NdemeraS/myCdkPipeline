d22a4542f782ef85759a45ac6deacee675774691
EC2 Default User <ec2-user@ip-172-31-24-10.simba.com>
2025-02-22 07:13:24 +0000

    adding ecs test

--- lib/my-pipeline-app-stage.ts
+++ lib/my-pipeline-app-stage.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from "constructs";
 import { MyLambdaStack } from './my-pipeline-lambda-stack';
+import { EcsTestStack } from './my-pipeline-ecs';
 
 export class MyPipelineAppStage extends cdk.Stage {
 
@@ -8,5 +9,8 @@ export class MyPipelineAppStage extends cdk.Stage {
       super(scope, id, props);
 
       const lambdaStack = new MyLambdaStack(this, 'LambdaStack');
+
+      const EcsTest = new EcsTestStack(this, 'ecsStack');
+      
     }
 }
---
+++ lib/my-pipeline-ecs.ts
@@ -0,0 +1,40 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import ecs = require('aws-cdk-lib/aws-ecs');
+import ec2 = require('aws-cdk-lib/aws-ec2');
+import elbv2 = require('aws-cdk-lib/aws-elasticloadbalancingv2');
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class EcsTestStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 2 });
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+
+    // Add capacity to it
+    cluster.addCapacity('DefaultAutoScalingGroupCapacity', {
+      instanceType: new ec2.InstanceType("t2.xlarge"),
+      desiredCapacity: 3,
+    });
+
+    const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef');
+
+    taskDefinition.addContainer('DefaultContainer', {
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      memoryLimitMiB: 1028,
+      memoryReservationMiB: 512,
+      // cpu: 1024,
+    });
+
+    // Instantiate an Amazon ECS Service
+    const ecsService = new ecs.Ec2Service(this, 'Service', {
+      cluster,
+      taskDefinition,
+      minHealthyPercent: 100,
+      desiredCount: 2,
+    });
+  
+  }
+}
